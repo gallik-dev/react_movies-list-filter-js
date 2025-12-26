f0632319a7f9d41d6928ef4eebc3cfa03dc3d20a
Maksym Halushko <gallikillag@gmail.com>
2025-12-26 18:06:23 +0200

    fix solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 function getPreparedMovies(movies, { query }) {
-  let preparedMovies = [...movies];
+  let preparedMovies = movies;
 
   if (query) {
     const normalizedQuery = query.trim().toLowerCase();
